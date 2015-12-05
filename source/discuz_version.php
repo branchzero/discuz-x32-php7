867f0ba92a17859940c8d071fdb386e5332b0e08
BranchZero Sun <branchzero@gmail.com>
2015-12-05 16:07:48 +0800

    Initialize Discuz_X3.2_SC_UTF8 20150609

---
+++ source/discuz_version.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: discuz_version.php 35307 2015-06-09 02:15:04Z hypowang $
+ */
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+if(!defined('DISCUZ_VERSION')) {
+	define('DISCUZ_VERSION', 'X3.2');
+	define('DISCUZ_RELEASE', '20150609');
+	define('DISCUZ_FIXBUG', '32000000');
+}
+
+?>
