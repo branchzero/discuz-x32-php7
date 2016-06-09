cf6c92915592d93b4f319f56975532337c0ba46c
BranchZero Sun <branchzero@gmail.com>
2016-06-09 18:22:11 +0800

    Fix Wrong Refer

--- source/plugin/mobile/api/2/uploadavatar.php
+++ source/plugin/mobile/api/2/uploadavatar.php
@@ -16,12 +16,12 @@ include_once 'home.php';
 
 class mobile_api {
 
-	var $tmpavatar;
-	var $tmpavatarbig;
-	var $tmpavatarmiddle;
-	var $tmpavatarsmall;
+	public static $tmpavatar;
+	public static $tmpavatarbig;
+	public static $tmpavatarmiddle;
+	public static $tmpavatarsmall;
 
-	function common() {
+	public static function common() {
 		global $_G;
 		if(empty($_G['uid'])) {
 			self::error('api_uploadavatar_unavailable_user');
@@ -62,19 +62,19 @@ class mobile_api {
 			self::error('api_uploadavatar_unusable_image');
 		}
 
-		$this->tmpavatar = $tmpavatar;
-		$this->tmpavatarbig = $avatarpath.$tmpavatarbig;
-		$this->tmpavatarmiddle = $avatarpath.$tmpavatarmiddle;
-		$this->tmpavatarsmall = $avatarpath.$tmpavatarsmall;
+		self::tmpavatar = $tmpavatar;
+		self::tmpavatarbig = $avatarpath.$tmpavatarbig;
+		self::tmpavatarmiddle = $avatarpath.$tmpavatarmiddle;
+		self::tmpavatarsmall = $avatarpath.$tmpavatarsmall;
 	}
 
-	function output() {
+	public static function output() {
 		global $_G;
 		if(!empty($_G['uid'])) {
-			if($this->tmpavatarbig && $this->tmpavatarmiddle && $this->tmpavatarsmall) {
-				$avatar1 = self::byte2hex(file_get_contents($this->tmpavatarbig));
-				$avatar2 = self::byte2hex(file_get_contents($this->tmpavatarmiddle));
-				$avatar3 = self::byte2hex(file_get_contents($this->tmpavatarsmall));
+			if(self::tmpavatarbig && self::tmpavatarmiddle && self::tmpavatarsmall) {
+				$avatar1 = self::byte2hex(file_get_contents(self::tmpavatarbig));
+				$avatar2 = self::byte2hex(file_get_contents(self::tmpavatarmiddle));
+				$avatar3 = self::byte2hex(file_get_contents(self::tmpavatarsmall));
 
 				$extra = '&avatar1='.$avatar1.'&avatar2='.$avatar2.'&avatar3='.$avatar3;
 				$result = self::uc_api_post_ex('user', 'rectavatar', array('uid' => $_G['uid']), $extra);
@@ -99,7 +99,7 @@ class mobile_api {
 		}
 	}
 
-	function byte2hex($string) {
+	public static function byte2hex($string) {
 		$buffer = '';
 		$value = unpack('H*', $string);
 		$value = str_split($value[1], 2);
@@ -111,7 +111,7 @@ class mobile_api {
 		return $b;
 	}
 
-	function uc_api_post_ex($module, $action, $arg = array(), $extra = '') {
+	public static function uc_api_post_ex($module, $action, $arg = array(), $extra = '') {
 		$s = $sep = '';
 		foreach($arg as $k => $v) {
 			$k = urlencode($k);
